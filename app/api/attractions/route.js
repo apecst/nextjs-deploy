37a8ed5aad580bbb3251f8fecd9f55f41ccebb25
Karn Yongsiriwit <xxx@xxx.com>
2025-03-24 14:30:42 +0700

    add api update attractions

--- app/api/attractions/route.js
+++ app/api/attractions/route.js
@@ -1,26 +1,41 @@
 import { NextResponse } from "next/server";
 import { mysqlPool } from "@/utils/db";
 
-const db=mysqlPool.promise()
+const db = mysqlPool.promise()
 
-export async function GET(request){
+export async function GET(request) {
     try {
         const [rows, fields] = await db.query(
             'SELECT * FROM attractions'
         )
-        return NextResponse.json(rows, {status: 200})
-    } catch (error){
-        return NextResponse.json({error:"Failed to fetch"}, {status: 500})
+        return NextResponse.json(rows, { status: 200 })
+    } catch (error) {
+        return NextResponse.json({ error: "Failed to fetch" }, { status: 500 })
     }
 }
-export async function POST(request){
+export async function POST(request) {
     try {
-        const {name, detail,coverimage, latitude, longitude} = await request.json();
+        const { name, detail, coverimage, latitude, longitude } = await request.json();
         const [result] = await db.query(
-            'INSERT INTO attractions (name, detail,coverimage, latitude, longitude) VALUES (?,?,?,?,?)',[name, detail,coverimage, latitude, longitude]
+            'INSERT INTO attractions (name, detail,coverimage, latitude, longitude) VALUES (?,?,?,?,?)', [name, detail, coverimage, latitude, longitude]
         );
-        return NextResponse.json({id:result.insertId,name, detail,coverimage, latitude, longitude}, {status: 200});
+        return NextResponse.json({ id: result.insertId, name, detail, coverimage, latitude, longitude }, { status: 200 });
+    } catch (error) {
+        return NextResponse.json({ error: error }, { status: 500 })
+    }
+}
+export async function PUT(request){
+    try {
+        const {id, name, detail, coverimage, latitude, longitude} = await request.json();
+        const [result] = await db.query(
+            'UPDATE attractions SET name=?, detail=?, coverimage=?, latitude=?, longitude=? WHERE id = ?',[name, detail,coverimage, latitude, longitude, id]
+        );
+        if (result.affectedRows == 0){
+            return NextResponse.json({error: "ID Not found"}, {status: 404});
+        }
+
+        return NextResponse.json({message: "Updated", id}, {status: 200});
     } catch (error){
-        return NextResponse.json({error:error}, {status: 500})
+        return NextResponse.json({error: error}, {status: 500})
     }
 }
